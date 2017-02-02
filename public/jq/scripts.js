761bf0eac76581d29cd77a5431c8d9876db36ed2
dustantb <dburr@getjive.com>
2017-02-01 21:36:12 -0700

    Fix scrolling for reals

--- public/jq/scripts.js
+++ public/jq/scripts.js
@@ -17,7 +17,7 @@
     $('a.page-scroll').bind('click', function(event) {
         var $ele = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($ele.attr('href')).offset().top)
+            scrollTop: ($($ele.attr('href')).offset().top - 59)
         }, 1450, 'easeInOutExpo');
         event.preventDefault();
     });
